c00ef439d8d782bc580e21aa8f8cf79bed311a79
LayneHaber <layne.haber@gmail.com>
2021-04-24 19:30:05 -0600

    Add simple node script

---
+++ index.js
@@ -0,0 +1,25 @@
+const {
+  createCoreTransferState,
+  getRandomBytes32,
+  encodeCoreTransferState,
+} = require("@connext/vector-utils");
+const { Tree } = require("vector-merkle-tree");
+
+const run = () => {
+  console.log("Creating tree");
+  const tree = new Tree();
+  tree.free();
+  console.log("Creating leaves");
+  const transfers = Array(10)
+    .fill(0)
+    .map((_) => createCoreTransferState({ transferId: getRandomBytes32() }));
+
+  const leaves = transfers.map((t) => encodeCoreTransferState(t));
+  console.log("Inserting leaves");
+  leaves.forEach((leaf) => {
+    tree.insert_hex_js(leaf);
+  });
+  console.log("Root", tree.root_js());
+};
+
+run();
